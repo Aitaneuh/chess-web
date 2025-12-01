6e50e06705e348edf5d0042781aa009662bbf4f5
Aitaneuh <ethanb9407@gmail.com>
2025-12-01 14:15:40 +0100

    ai can now detected mate in 1

--- server/ai_agent.py
+++ server/ai_agent.py
@@ -1,3 +1,5 @@
+from cmath import inf
+import time
 import chess
 import chess.polyglot
 
@@ -5,22 +7,34 @@ class AIAgent:
     def __init__(self):
         self.simulated_moves = 0
 
-    # Piece values (centipawns)
+    # using chess programming wiki values
     PIECE_VALUES = {
-        chess.PAWN: 100,
-        chess.KNIGHT: 320,
-        chess.BISHOP: 330,
-        chess.ROOK: 500,
-        chess.QUEEN: 900,
-        chess.KING: 20000
+        chess.PAWN: 10,
+        chess.KNIGHT: 30,
+        chess.BISHOP: 30,
+        chess.ROOK: 50,
+        chess.QUEEN: 90,
+        chess.KING: 900
     }
 
-    def play(self, board: chess.Board) -> str:
+    def play(self, board: chess.Board, depth: int) -> str:
+        start = time.time()
         try:
             with chess.polyglot.open_reader("Titans.bin") as book:
                 entry = book.find(board)
+                execTime = time.time() - start
+                print("Execution time:", execTime, "s")
                 return entry.move.uci()
         except:
             pass
 
+        
+        for move in board.legal_moves:
+            analysis_board = board.copy()
+            analysis_board.push(move)
+            if analysis_board.is_checkmate():
+                return move.uci()
+
+        execTime = time.time() - start
+        print("Execution time:", execTime, "s")
         return ""
